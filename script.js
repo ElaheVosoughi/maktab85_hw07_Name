8bb9218437427b892fbd64973fa1ae5b39a2bba9
ElaheVosoughi <elahe.vosoghi0919@gmail.com>
2022-11-01 23:21:41 +0330

    ex1 solved

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-"use strict"
+"use strict";
+const topSalary = function () {
+  let salaries = {
+    John: 100,
+    Pete: 300,
+    Mary: 250,
+  };
+
+  const converted = Object.entries(salaries);
+  const [john, pete, mary] = converted;
+
+  let biggest = 0;
+  if (john[1] > pete[1]) {
+    biggest = john;
+  } else {
+    biggest = pete;
+  }
+  if (biggest[1] < mary[1]) {
+    biggest = mary;
+  }
+  return biggest;
+};
+
+console.log(`TOP SALARY BELONGS TO : ${topSalary()}`);

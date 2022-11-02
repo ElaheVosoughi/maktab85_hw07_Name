f682bc7593197134b2be967b8853cb320aa2f055
ElaheVosoughi <elahe.vosoghi0919@gmail.com>
2022-11-02 23:12:45 +0330

    hw7-ex2 solved

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
-"use strict";
-const topSalary = function () {
-  let salaries = {
-    John: 100,
-    Pete: 300,
-    Mary: 250,
-  };
+// "use strict";
 
-  const converted = Object.entries(salaries);
-  const [john, pete, mary] = converted;
+//------------answer 1 -------------
 
-  let biggest = 0;
-  if (john[1] > pete[1]) {
-    biggest = john;
+// const topSalary = function () {
+//   let salaries = {
+//     John: 100,
+//     Pete: 300,
+//     Mary: 250,
+//   };
+
+//   const converted = Object.entries(salaries);
+//   const [john, pete, mary] = converted;
+
+//   let biggest = 0;
+//   if (john[1] > pete[1]) {
+//     biggest = john;
+//   } else {
+//     biggest = pete;
+//   }
+//   if (biggest[1] < mary[1]) {
+//     biggest = mary;
+//   }
+//   return biggest;
+// };
+
+// console.log(`TOP SALARY BELONGS TO : ${topSalary()}`);
+
+//-------------------------------------
+
+//---------------answer 2--------------
+let userInput = [];
+const promptUser = function () {
+  let getArr = prompt("insert your array");
+  if (getArr !== " ") {
+    getArr = getArr.split(",");
+    userInput.push(getArr);
+    promptUser();
   } else {
-    biggest = pete;
+    console.log(userInput);
   }
-  if (biggest[1] < mary[1]) {
-    biggest = mary;
-  }
-  return biggest;
 };
 
-console.log(`TOP SALARY BELONGS TO : ${topSalary()}`);
+promptUser();
+
+let newArr = [];
+const sortArr = function (arrayOfArrays) {
+  arrayOfArrays.forEach((item) => {
+    newArr = newArr.concat(item);
+  });
+  return newArr;
+};
+
+console.log(sortArr(userInput).sort((a, b) => b-a));
